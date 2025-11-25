47cd061ee446ffac0cb59468e0a32ed2e18566d9
Reshala5739 <Max.san.2005@mail.ru>
2025-11-26 02:39:39 +0700

    Commit 9

--- Todo.Core/TodoItem.cs
+++ Todo.Core/TodoItem.cs
@@ -4,6 +4,12 @@
 // </copyright>
 // -------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Todo.Core
 {
     public class TodoItem
